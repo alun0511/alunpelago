5b1d60f4117a9812a1d01b975437be98079540f4
alun0511 <78014508+alun0511@users.noreply.github.com>
2022-12-19 15:13:42 +0100

    PHP-file setup
    Added calendar and initiated in index.php
    added php-files to begin adding logic

---
+++ calendar.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use benhall14\phpCalendar\Calendar as Calendar;
+
+
+$calendar = new Calendar();
--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-<h1>Hello World!</h1>
+<?php
+
+require __DIR__ . '/calendar.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alunpelago</title>
+</head>
+
+<body>
+    <?= $calendar->draw(date('2023-01-01')) ?>
+
+    <form action="" methord="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" class="form-input">
+        <label for="arrival">Date of arrival:</label>
+        <input type="date" name="arrival" class="form-input" min="2023-01-01" max="2023-01-31">
+        <label for="departure">Date of departure:</label>
+        <input type="date" name="departure" class="form-input" min="2023-01-01" max="2023-01-31">
+    </form>
+
+</body>
+
+</html>
